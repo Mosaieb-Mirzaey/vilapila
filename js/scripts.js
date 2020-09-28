6315c7a8c07c21a4ff6e2e401e7cfa6f50861ec6
Mosaieb-Mirzaey <mosaieb.mirzaey@gmail.com>
2020-09-28 16:53:50 +0330

    Finish Calendar & setUp CounterTop

--- js/scripts.js
+++ js/scripts.js
@@ -1,104 +1,202 @@
 
 // *** Persian Calendar ***
-
-$('#input-arrival-time,#input-departure-time').each(function(){
-    $(this).persianDatepicker({
-        autoClose: true,
-        "inline": false,
-        "format": "LL",
-        "viewMode": "day",
-        "initialValue": true,
-        "minDate": 1601264959074,
-        "maxDate": 1651964959074,
-        "position": "auto",
-        "altFormat": "ll",
-        "altField": "#departure-time",
-
-
-
-
-        "onlyTimePicker": false,
-        "onlySelectOnDate": true,
-        "calendarType": "persian",
-        "inputDelay": 800,
-        "observer": false,
-        "calendar": {
-            "persian": {
-                "locale": "fa",
-                "showHint": true,
-                "leapYearMode": "algorithmic"
-            },
-            "gregorian": {
-                "locale": "en",
-                "showHint": true
-            }
-        },
-        "navigator": {
-            "enabled": true,
-            "scroll": {
-                "enabled": true
-            },
-            "text": {
-                "btnNextText": "<",
-                "btnPrevText": ">"
-            }
-        },
-        "toolbox": {
-            "enabled": true,
-            "calendarSwitch": {
-                "enabled": true,
-                "format": "MMMM"
-            },
-            "todayButton": {
-                "enabled": true,
-                "text": {
-                    "fa": "امروز",
-                    "en": "Today"
+$("#input-arrival-time").click(
+    function () {
+        $("#input-arrival-time").persianDatepicker({
+            autoClose: true,
+            "inline": false,
+            "format": "LL",
+            "viewMode": "day",
+            "initialValue": true,
+            "minDate": 1601264959074,
+            "maxDate": 1651964959074,
+            "position": "auto",
+            "altFormat": " dddd  DD / MMM /YYYY",
+            "altField": "#arrival-time",
+            "onlyTimePicker": false,
+            "onlySelectOnDate": true,
+            "calendarType": "persian",
+            "inputDelay": 800,
+            "observer": false,
+            "calendar": {
+                "persian": {
+                    "locale": "fa",
+                    "showHint": true,
+                    "leapYearMode": "algorithmic"
+                },
+                "gregorian": {
+                    "locale": "en",
+                    "showHint": true
                 }
             },
-            "submitButton": {
+            "navigator": {
                 "enabled": true,
+                "scroll": {
+                    "enabled": true
+                },
                 "text": {
-                    "fa": "تایید",
-                    "en": "Submit"
+                    "btnNextText": "<",
+                    "btnPrevText": ">"
                 }
             },
-            "text": {
-                "btnToday": "امروز"
-            }
-        },
-        "timePicker": {
-            "enabled": true,
-            "step": 1,
-            "hour": {
-                "enabled": false,
-                "step": null
+            "toolbox": {
+                "enabled": true,
+                "calendarSwitch": {
+                    "enabled": true,
+                    "format": "MMMM"
+                },
+                "todayButton": {
+                    "enabled": true,
+                    "text": {
+                        "fa": "امروز",
+                        "en": "Today"
+                    }
+                },
+                "submitButton": {
+                    "enabled": true,
+                    "text": {
+                        "fa": "تایید",
+                        "en": "Submit"
+                    }
+                },
+                "text": {
+                    "btnToday": "امروز"
+                }
             },
-            "minute": {
-                "enabled": false,
-                "step": null
+            "timePicker": {
+                "enabled": true,
+                "step": 1,
+                "hour": {
+                    "enabled": false,
+                    "step": null
+                },
+                "minute": {
+                    "enabled": false,
+                    "step": null
+                },
+                "second": {
+                    "enabled": false,
+                    "step": null
+                },
+                "meridian": {
+                    "enabled": false
+                }
             },
-            "second": {
-                "enabled": false,
-                "step": null
+            "dayPicker": {
+                "enabled": true,
+                "titleFormat": "YYYY MMMM"
             },
-            "meridian": {
-                "enabled": false
-            }
-        },
-        "dayPicker": {
-            "enabled": true,
-            "titleFormat": "YYYY MMMM"
-        },
-        "monthPicker": {
-            "enabled": true,
-            "titleFormat": "YYYY"
-        },
-        "yearPicker": {
-            "enabled": true,
-            "titleFormat": "YYYY"
-        },
-        "responsive": true
+            "monthPicker": {
+                "enabled": true,
+                "titleFormat": "YYYY"
+            },
+            "yearPicker": {
+                "enabled": true,
+                "titleFormat": "YYYY"
+            },
+            "responsive": true
 
-    });
-})
+        });
+    }
+    )
+
+
+$("#input-departure-time").click(
+    function (){
+        $("#input-departure-time").persianDatepicker({
+            autoClose: true,
+            "inline": false,
+            "format": "LL",
+            "viewMode": "day",
+            "initialValue": true,
+            "minDate": 1601264959074,
+            "maxDate": 1651964959074,
+            "position": "auto",
+            "altFormat": " dddd  DD / MMM /YYYY",
+            "altField": "#departure-time",
+            "onlyTimePicker": false,
+            "onlySelectOnDate": true,
+            "calendarType": "persian",
+            "inputDelay": 800,
+            "observer": false,
+            "calendar": {
+                "persian": {
+                    "locale": "fa",
+                    "showHint": true,
+                    "leapYearMode": "algorithmic"
+                },
+                "gregorian": {
+                    "locale": "en",
+                    "showHint": true
+                }
+            },
+            "navigator": {
+                "enabled": true,
+                "scroll": {
+                    "enabled": true
+                },
+                "text": {
+                    "btnNextText": "<",
+                    "btnPrevText": ">"
+                }
+            },
+            "toolbox": {
+                "enabled": true,
+                "calendarSwitch": {
+                    "enabled": true,
+                    "format": "MMMM"
+                },
+                "todayButton": {
+                    "enabled": true,
+                    "text": {
+                        "fa": "امروز",
+                        "en": "Today"
+                    }
+                },
+                "submitButton": {
+                    "enabled": true,
+                    "text": {
+                        "fa": "تایید",
+                        "en": "Submit"
+                    }
+                },
+                "text": {
+                    "btnToday": "امروز"
+                }
+            },
+            "timePicker": {
+                "enabled": true,
+                "step": 1,
+                "hour": {
+                    "enabled": false,
+                    "step": null
+                },
+                "minute": {
+                    "enabled": false,
+                    "step": null
+                },
+                "second": {
+                    "enabled": false,
+                    "step": null
+                },
+                "meridian": {
+                    "enabled": false
+                }
+            },
+            "dayPicker": {
+                "enabled": true,
+                "titleFormat": "YYYY MMMM"
+            },
+            "monthPicker": {
+                "enabled": true,
+                "titleFormat": "YYYY"
+            },
+            "yearPicker": {
+                "enabled": true,
+                "titleFormat": "YYYY"
+            },
+            "responsive": true
+
+        })
+    }
+)
